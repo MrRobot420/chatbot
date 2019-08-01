d15370fad7bc63c9dc2452a1b5bcc5067f8d8e2b
MrRobot420 <max.karlchen@web.de>
2019-08-01 14:13:54 +0200

    Added auto scroll functionality

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       sender: 'user',
       color: '#61dafb'
     }
+    this.textArea = React.createRef();
   }
 
   sendMessage(app) {
@@ -20,11 +21,12 @@ class App extends Component {
       var input = this.state.conversation + "You: " + this.state.inputValue + "\n\n";
       var current_input = this.state.inputValue;
       this.setState({conversation: input});
+      this.setState({inputValue: ""});
+      this.scrollDown();
       console.log(`User typed: ${ input }`);
       const headers = new Headers();
       headers.append('Content-Type', 'application/json');
-
-      var response = fetch('http://localhost:5000/api', {
+      fetch('http://localhost:5000/api', {
         method: "post",
         headers: headers,
         body: JSON.stringify({
@@ -32,15 +34,18 @@ class App extends Component {
           userQuery: current_input
         })
       }).then(function(res) {
-        console.log(res);
         return res.json();
       }).then((data) => {
         input = this.state.conversation + "Bot: " + data.response + "\n\n";
         this.setState({conversation: input});
+        this.scrollDown();
       })
-
-      
     } 
+  }
+
+  scrollDown(app) {
+    console.log("I would like to scroll down!");
+    this.refs.textArea.scrollTop = this.refs.textArea.scrollHeight;
   }
 
   updateInputValue(evt) {
@@ -56,7 +61,7 @@ class App extends Component {
         <header className="App-header">
           <div className="App-chat">
             <h1>Chat Bot</h1>
-            <textarea value={this.state.conversation} className="App-textfield"></textarea><br></br>
+            <textarea ref="textArea" value={this.state.conversation} className="App-textfield"></textarea><br></br>
             <input value={this.state.inputValue} onKeyDown={app => this.sendMessage(app)} onChange={evt => this.updateInputValue(evt)} className="App-input"></input><br></br>
             <button id="send" className="App-send" onClick={app => this.sendMessage(app)}>Send</button>
           </div>
